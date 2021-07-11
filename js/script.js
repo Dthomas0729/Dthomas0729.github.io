a3f6f5145a470c4013e757ebd8d387dd2335b482
Dominique Thomas <63202540+Dthomas0729@users.noreply.github.com>
2021-07-11 16:25:35 -0400

    move files from HangmanCinemas folder and delete folder

--- js/script.js
+++ js/script.js
@@ -239,36 +239,9 @@ let hangmanCinemas = {
         $('#winScreen').fadeIn("slow");
         $('#gameplayScreen').hide();
 
-
+        // MOVIE POSTER IMAGE CREATION
         $('div#moviePoster').html(`<img src="${hangmanCinemas.movieInPlay.posterUrl}">`);
-        
-        $(document).ready(function() {
-            $('#moviePoster img').each(function() {
-                var maxWidth = 320; // Max width for the image
-                var maxHeight = 480;    // Max height for the image
-                var ratio = 0;  // Used for aspect ratio
-                var width = $(this).width();    // Current image width
-                var height = $(this).height();  // Current image height
             
-                // Check if the current width is larger than the max
-                if(width > maxWidth){
-                    ratio = maxWidth / width;   // get ratio for scaling image
-                    $(this).css("width", maxWidth); // Set new width
-                    $(this).css("height", height * ratio);  // Scale height based on ratio
-                    height = height * ratio;    // Reset height to match scaled image
-                }
-            
-                // Check if current height is larger than max
-                if(height > maxHeight){
-                    ratio = maxHeight / height; // get ratio for scaling image
-                    $(this).css("height", maxHeight);   // Set new height
-                    $(this).css("width", width * ratio);    // Scale width based on ratio
-                    width = width * ratio;    // Reset width to match scaled image
-                }
-            })
-        });
-            
-
         $('#movieTitle').text(hangmanCinemas.movieInPlay.title);
         $('#movieDetails').html(`
             <h3 id='movieTitle'>${hangmanCinemas.movieInPlay.title}</h3>
